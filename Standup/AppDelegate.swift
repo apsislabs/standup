a3e674e422acd703aea2833ae55363893837fdfa
Wyatt Kirby <kirby.wa@gmail.com>
2016-03-15 17:21:17 -0700

    Initial Commit

---
+++ Standup/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Standup
+//
+//  Created by Wyatt Kirby on 3/15/16.
+//  Copyright © 2016 Apsis Labs. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
